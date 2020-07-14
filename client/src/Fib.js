70cfbca5cfbfdc506c806de5768b9331f47cbb28
medsaidraoudh <saidhamouda67@yahoo.fr>
2020-07-14 12:36:13 +0100

    never

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -60,7 +60,7 @@ class Fib extends Component {
           <button>Submit</button>
         </form>
 
-        <h3>Indexes I have seen:</h3>
+        <h3>Indexes I have never seen:</h3>
         {/* {this.renderSeenIndexes()} */}
 
         <h3>Calculated Values:</h3>
